40bb5881648e4a10c1497f54c04e519d3c19edba
Charles <charles.murphy.042@gmail.com>
2023-03-11 14:35:06 -0500

    Basic menu

---
+++ Assets/Scripts/Utils/SceneLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace kingcrimson.utils
+{
+    public class SceneLoader : MonoBehaviour
+    {
+        [SerializeField] private string m_sceneToLoad;
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(m_sceneToLoad);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();   
+        }
+    }
+}
