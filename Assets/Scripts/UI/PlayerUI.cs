6323801c0541d047ceb0759eac043af419547f0a
Charles <charles.murphy.042@gmail.com>
2023-03-12 01:18:26 -0400

    Bar flash

--- Assets/Scripts/UI/PlayerUI.cs
+++ Assets/Scripts/UI/PlayerUI.cs
@@ -12,10 +12,11 @@ namespace kingcrimson.ui
         [SerializeField] private TextMeshProUGUI m_wokenessStateLabel;
         [SerializeField] private Player m_player;
 
+        [SerializeField] private Image m_barFill;
+
 
         private void Awake()
         {
-            
         }
 
         private void Start()
@@ -23,11 +24,36 @@ namespace kingcrimson.ui
             m_player.OnWokenessChange.AddListener(UpdateWokenessBar);
             m_wokenessBar.maxValue = m_player.Wokeness;
             m_wokenessBar.value = m_player.Wokeness;
+
+            StartCoroutine(WokebarFlash());
         }
 
         private void UpdateWokenessBar(float total, float change)
         {
             m_wokenessBar.value = total;
+        }
+
+        private IEnumerator WokebarFlash()
+        {
+            while (true)
+            {
+                float flashDuration = m_wokenessBar.value / (m_wokenessBar.maxValue - 1);
+                if (flashDuration < 1)
+                {
+                    Color c = m_barFill.color;
+                    c.a = 0;
+                    m_barFill.color = c;
+                    yield return new WaitForSeconds(flashDuration * 0.25f);
+
+                    c.a = 1;
+                    m_barFill.color = c;
+                    yield return new WaitForSeconds(flashDuration * 0.75f);
+                }
+                else
+                {
+                    yield return null;
+                }
+            }
         }
     }
 }
